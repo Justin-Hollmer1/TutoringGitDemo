c5bfb60642a1a386c46d9a526fb32490fd837b1e
Justin Hollmer <justin.hollmer@gmail.com>
2023-10-25 07:45:01 -0500

    added another thing

--- pythonStuff.py
+++ pythonStuff.py
@@ -1,9 +1,3 @@
-#import math
-
-userInput = input()
-
-# if (userInput == "test"):
-#     print('You entered the word: "test"')
 
 condition = True
 while (condition):
@@ -17,37 +11,4 @@ print("successfully exited")
 
 thisIsCamelCase = "camel case"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Function definition
-#---------------------------------------------------------
-# def addTwoNumbers(num1, num2):
-#     addedNumbers = num1 + num2
-#     return addedNumbers
-# #---------------------------------------------------------
-
-# sumOfTheTwoNumbers = addTwoNumbers(2, 3) # will equal 5
-# print(sumOfTheTwoNumbers)
-# number = 1
-
-# while (number <= 100): # number == 11
-#     for i in range(2, number): # will loop from 2 - 11
-#         if (number % i == 0): #if the number / i has a remainder:
-#             # This will be true if the number is composite
-#             break
-#     else:
-#         print(number)
-#     number += 1
+#testing
